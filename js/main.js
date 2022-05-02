4e9ed37a9be90fb7153b5686d0af5f737ca89538
kevinhalliwell <godzirrasti@gmail.com>
2022-05-02 19:24:54 -0400

    updates
    started working on game constructor

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// create tic tac toe object
+class TicTacToe{
+    constructor(player1, player2){
+        this.player1 = player1
+        this.player2 = player2
+
+        // create game board, empty = 0, X = 1, O = 2
+        this.gameBoard = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+        ]
+
+        // variable to check gameover
+        this.gameOver = false
+    }
+
+}
+
+// start game
+
+// check score
+
+// check player turn
+
+// check win or lose
+
+// check local storage for record
+
+// enter names of players? could store in local storage
+
+// could make a leader board with names and scores etc.... local storage only
