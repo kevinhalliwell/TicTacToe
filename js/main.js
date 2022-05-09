4a2ca66983a4bc51d48d68a362a0a576fc231bab
Kevin Halliwell <97984636+kevinhalliwell@users.noreply.github.com>
2022-05-09 15:34:37 -0400

    updates
    game is working!

--- js/main.js
+++ js/main.js
@@ -8,21 +8,21 @@ class TicTacToe{
         this.playerTwo = []
         this.playerOneTurn = false
         this.playerTwoTurn = false
-        checkWin: [
-            // Rows
-            ['0', '1', '2'],
-            ['3', '4', '5'],
-            ['6', '7', '8'],
+        // this.checkWin; [
+        //     // Rows
+        //     ['0', '1', '2'],
+        //     ['3', '4', '5'],
+        //     ['6', '7', '8'],
         
-            // Columns
-            ['0', '3', '6'],
-            ['1', '4', '7'],
-            ['2', '5', '8'],
+        //     // Columns
+        //     ['0', '3', '6'],
+        //     ['1', '4', '7'],
+        //     ['2', '5', '8'],
         
-            // Diagonal
-            ['0', '4', '8'],
-            ['2', '4', '6']
-        ]
+        //     // Diagonal
+        //     ['0', '4', '8'],
+        //     ['2', '4', '6']
+        // ]
     }
     // start game
     // function to determine witch player will start the game
@@ -38,29 +38,60 @@ class TicTacToe{
         }
     }
 
+    // check the win
+    checkWin(player){
+        let win = false
+         let winningStates = [
+            // winning conditions contained in the rows
+            ['0', '1', '2'],
+            ['3', '4', '5'],
+            ['6', '7', '8'],
+        
+            // winning conditions contained in the columns
+            ['0', '3', '6'],
+            ['1', '4', '7'],
+            ['2', '5', '8'],
+        
+            // winning condition contained in the diagonal
+            ['0', '4', '8'],
+            ['2', '4', '6']
+        ]
+        let res = winningStates.filter(p => p.filter(s => {
+            return player.indexOf(s) > -1;
+          }).length == 3);
+          if (res.length > 0){
+              win = true
+              return win
+          }
+    }
+
     // main logic function. can be refactored and made smaller, more functions etc
     move(){
         document.addEventListener('click', event => {
             let target = event.target
             let cell = target.classList.contains('box')
             let selected = target.classList.contains('selected')
-            
+
             if (cell && !selected) {
                 const playerSelection = target.dataset.value
-                
-                // FIX THESE VARIABLES HERE**********
+                // check to see which player is up, enter the statement and change player
                 if (this.playerOneTurn === true && this.playerTwoTurn === false) {
                     this.playerOneTurn = false
                     this.playerTwoTurn = true
+                    // update class to include x allows for X placement
                     target.classList.add('x')
+                    // update player array for comparison to winning array
                     this.playerOne.push(playerSelection)
                 } else if (this.playerTwoTurn === true && this.playerOneTurn === false) {
                     this.playerTwoTurn = false
                     this.playerOneTurn = true
+                    // update class to include o allows for O placement
                     target.classList.add('o')
+                    // update player array for comparison to winning array
                     this.playerTwo.push(playerSelection)
                 }
-            
+                
+                // add selected to box class that was selected
                 target.classList.add('selected')
                     
                 // If all cells are selected, then its a draw print to the footer area
@@ -70,39 +101,15 @@ class TicTacToe{
                     document.querySelector('.footer').textContent = 'It\'s A Draw!'
                     console.log('Draw')
                 }
-        
-                TicTacToe.checkWin.forEach(checkWin => {
-                    let playerOneWin = winningState.every(s => TicTacToe.playerOne.includes(s))
-                    let playerTwoWin = winningState.every(s => TicTacToe.playerTwo.includes(s))
-            
-                    if (playerOneWin || playerTwoWin) {
-                        document.querySelectorAll('#box').forEach(cell => cell.classList.add('selected'))
-                        // havent sort the css for this yet
-                        // document.querySelector('.hidden').classList.add('visible')
-                        if (playerOneWin){
-                            document.querySelector('.footer').textContent = 'Player 1 WINS!!!'
-                        } else {
-                            document.querySelector('.footer').textContent = 'Player 2 WINS!!!'
-                        }
-                    }
-                })
+                if (this.checkWin(this.playerOne) === true) {
+                    document.querySelector('.footer').textContent = 'Player 1 WINS!!!'
+                } else if (this.checkWin(this.playerTwo) === true){
+                    document.querySelector('.footer').textContent = 'Player 2 WINS!!!'
+                }
             }
         })
     }
 }
-
-
-// // function to return the turn
-// function playerTurn(player1, player2) {
-//     if (this.player1 === true) {
-//         return this.player1
-//     } else if (this.player2 === true){
-//         return this.player2
-//     } else {
-//         console.log('error in the player function both players are false')
-//     }
-// }
-
 
 // still testing. array push is not working
 let game = new TicTacToe("J", "K")
